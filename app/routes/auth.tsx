21bd2e0f8e2e8d258b399f90a331e6b51a241b57
Иван Алексеевич Коледов <ivan.koledov@chipdip.ru>
2026-02-17 14:36:52 +0300

    added auth

---
+++ app/routes/auth.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
+export const meta = () => {
+  [
+    { title: "Resumind | Auth" },
+    { name: "description", content: "Log into your account" },
+  ];
+};
+
+const Auth = () => {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate(next);
+  }, [auth.isAuthenticated, next]);
+
+  return (
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+          <h1 className="flex items-center gap-2 text-center">Welcome</h1>
+          <h2>Log in to continue</h2>
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                Signing you in...
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>Log out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>Log in</p>
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Auth;
